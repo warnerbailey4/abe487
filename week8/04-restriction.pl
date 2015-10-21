c3ce9f2e2fbcea9e2363ef1354fd318d5861a4ff
Bailey Warner <warnerbailey4@email.arizona.edu>
2015-10-21 13:46:55 -0700

    working

--- week8/04-restriction.pl
+++ week8/04-restriction.pl
@@ -2,8 +2,17 @@
 
 use strict;
 use warnings;
+use autodie;
 use feature 'say';
 
-open $fh, '>', 
-my seq =~ s/([AT])(AATT[CG])/g;
-    
+my $file = shift or die "Please provide a sequence or file.\n";
+open my $fh, '<', $file;
+
+my $seq = '';
+while(my $line = <$fh>) {
+    chomp($line);
+    $seq .= $line;
+}    
+
+$seq =~ s/([AG])(AATT[CG])/$1^$2/g;
+say $seq;
